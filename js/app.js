ab2b5337882bd3aab9f36bdac462993039343acc
shakibnymr <shakibnymr11@gmail.com>
2023-08-26 22:59:09 +0600

    important

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('app')
+function loadData2 () {
+    fetch ('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+
+function loadUsers () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data) );
+}
+
+function displayUsers (data) {
+    const ul = document.getElementById('users-list');
+   for(const user of data){
+    console.log(user.name)
+    const li = document.createElement('li');
+    li.innerText = user.name;
+    ul.appendChild(li);
+   }
+    
+}
---
+++ js/post.js
@@ -0,0 +1,22 @@
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data));
+}
+
+function displayPosts (posts){
+    const postsContainer = document.getElementById('posts-container');
+    for(const post of posts){
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post')
+        console.log(post);
+        postDiv.innerHTML = `
+        <h4>User- ${post.userId} </h4>
+<h5>Post: ${post.title} titles</h5>
+<p>Post Description: ${post.body} </p>
+        `;
+        postsContainer.appendChild(postDiv);
+    }
+}
+
+loadData();
